84be8e5ba55b77e240eb12d1d019f89b1439ba13
liziqi <liziqi@wisdomgarden.com>
2020-12-01 12:01:44 +0800

    first commit

---
+++ src/test/java/com/fw/studyplatform/StudyPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fw.studyplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
